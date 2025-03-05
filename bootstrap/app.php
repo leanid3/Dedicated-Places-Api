f3b8e2902875322fc0e6189c70c1f1cbf600e3f3
leanid3 <144019647+leanid3@users.noreply.github.com>
2025-03-05 08:37:10 +0300

    debug api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,26 +8,27 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
-//        $middleware->append(\App\Http\Middleware\AdminCheckMiddleware::class);
-
+        $middleware->api([VerifyCsrfToken::class]);
+        //        $middleware->append(\App\Http\Middleware\AdminCheckMiddleware::class);
+    
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function(\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, Request $request){
-           if ($request->is('api/*')) {
-               return response()->json(['error' => 'Not Found'], 404);
-           }
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['error' => 'Not Found'], 404);
+            }
         });
     })
     ->create();
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return json_encode($request->user());
 });
-
 
 //admin
 require __DIR__ . '/API/admin.php';
