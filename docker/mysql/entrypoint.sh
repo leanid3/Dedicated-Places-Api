0659954a91a93c91dde73e45613de5065f12ebd0
leanid3 <144019647+leanid3@users.noreply.github.com>
2025-03-26 19:46:46 +0300

    правим бд

--- docker/mysql/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-# Автоматически исправляем права на папку с данными
-chown -R 999:999 /var/lib/mysql
-
-# Запускаем оригинальный entrypoint из образа MySQL
-exec /usr/local/bin/docker-entrypoint.sh "$@"
